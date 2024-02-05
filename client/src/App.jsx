b6fe3319a7d6ea2f9719ba30c1b0488dcb6d286b
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-02-05 12:42:45 +0530

    client setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import React from 'react'
 import io from 'socket.io-client'
 import ChatComp from './component/ChatComp'
+import joinMusic from './audio/notification.mp3'
 
 
 
@@ -11,29 +12,38 @@ const socket = io.connect("http://localhost:2000")
 const App = () => {
   const [username, setUsername] = useState("")
   const [room, setRoom] = useState("")
-  
-  const joinChat = ()=>{
-    if(username !== "" && room !== "" ){
-      socket.emit("join_room" , room)
+  const [showChat, setShowChat] = useState(false)
+
+  const notification = new Audio(joinMusic)
+
+  const joinChat = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room)
+      setShowChat(true)
+      notification.play()
     }
   }
 
   return (
     <>
-      <div className="join_room">
+      {!showChat && (<div className="join_room">
         <h1>Join Chat</h1>
 
-        <input type="text" placeholder='Enter Your Name' 
-        onChange= {(e)=>setUsername(e.target.value)} 
-        name="" id="" />
-        
-        <input type="text" placeholder='Enter Chat Room' 
-        onChange= {(e)=>setRoom(e.target.value)}
-        name="" id="" />
-        <button onClick={joinChat}>Join</button>
-      </div>
+        <input type="text" placeholder='Enter Your Name'
+          onChange={(e) => setUsername(e.target.value)}
+          name="" id="" />
 
-      <ChatComp socket = {socket} username = {username} room = {room}/>
+        <input type="text" placeholder='Enter Chat Room'
+          onChange={(e) => setRoom(e.target.value)}
+          onKeyPress={(e) => { e.key === "Enter" && joinChat()}} />
+        <button onClick={joinChat}>Join</button>
+      </div>)}
+
+      {
+        showChat && (
+          <ChatComp socket={socket} username={username} room={room} />
+        )
+      }
     </>
   )
 }
--- client/src/component/ChatComp.jsx
+++ client/src/component/ChatComp.jsx
@@ -1,35 +1,82 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import receiveMusic from '../audio/ding.mp3'
 
-const ChatComp = ({socket, username, room}) => {
+
+const ChatComp = ({ socket, username, room }) => {
 
   const [currentMessage, setcurrentMessage] = useState("")
+  const [messageList, setMessageList] = useState([])
 
-  const sendMessage = async ()=>{
-    if(currentMessage !== ""){
+  const notification = new Audio(receiveMusic)
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
       const messageData = {
-        id : Math.random(),
-        room : room,
-        auther : username,
-        message : currentMessage,
-        time : new Date(Date.now()).getHours()/24 + ":" + new Date(Date.now()).getMinutes()
+        id: Math.random(),
+        room: room,
+        author: username,
+        message: currentMessage,
+        time: new Date(Date.now()).getHours() % 12 + ":" + new Date(Date.now()).getMinutes()
       }
+
+      await socket.emit("send_message", messageData)
+      setMessageList((list) => [...list, messageData])
+      setcurrentMessage("");
+      notification.play()
+
     }
-    
+
   }
+
+  useEffect(() => {
+    const handleReceiveMessage = (data) => {
+      setMessageList((list) => [...list, data])
+    }
+    socket.on("receive_message", handleReceiveMessage)
+
+    return () => {
+      socket.off("receive_message", handleReceiveMessage)
+    }
+  }, [socket])
+
+
+  const containRef = useRef(null)
+  useEffect(() => {
+
+    containRef.current.scrollTop = containRef.current.scrollHeight
+  }, [messageList])
 
   return (
     <>
-    <div className="chat_container">
+      <div className="chat_container">
         <h1>Welcome {username} </h1>
         <div className="chat_box">
+          <div className="auto-scrolling-div" ref={containRef} style={{ height: '450px', overflowY: "auto", border: "2px solid red" }}>
 
-            <div className="chat_body">
-              <input type="text" value={currentMessage} placeholder='Type Your Message' name="" id="" onChange={(e)=>setcurrentMessage(e.target.value)} />
-              <button>&#9658;</button>
-            </div>
+            {
+              messageList.map((data) => (
+                <div key={data.id} className="message_content" id={username == data.author ? "you" : "other"} >
+                  <div>
+                    <div className="msg" id={username == data.author ? "y" : "b"}>
+                      <p>{data.message}</p>
+                    </div>
+                    <div className="msg_details">
+                      <p>{data.author}</p>
+                      <p>{data.time}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+
+          <div className="chat_body">
+            <input type="text" value={currentMessage} placeholder='Type Your Message' name="" id="" onChange={(e) => setcurrentMessage(e.target.value)} onKeyPress={(e) => { e.key === "Enter" && sendMessage() }} />
+            <button onClick={sendMessage}>&#9658;</button>
+          </div>
 
         </div>
-    </div>
+      </div >
     </>
   )
 }
